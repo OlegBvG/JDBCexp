cf428eb3908e2188fecb145b421e36a123639af3
OlegBvG <borisov_og@mail.ru>
2019-10-02 19:57:21 +0300

    SELECT avg another logic

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,15 +9,23 @@ public class Main {
         Connection connection = DriverManager.getConnection( url,  user,  pass );
         Statement statement = connection.createStatement();
         ResultSet resultSet = statement.executeQuery("" +
-                "select p1.course_name name, avg(amtCourse) avgPurchase  from skillbox.purchaselist p1, \n" +
-                "(select distinctrow course_name, month(subscription_date) monthNum, count(course_name) amtCourse  \n" +
-                        "from skillbox.purchaselist group by course_name, monthNum) p2\n" +
-                "where p1.course_name = p2.course_name and month(p1.subscription_date) = monthNum\n" +
-                "group by p1.course_name;");
+                "SELECT  p.course_name name\n" +
+                ", max(p.subscription_date) mx\n" +
+                ", min(p.subscription_date) mn\n" +
+                ", count(p.course_name) / \n" +
+                "  (period_diff(DATE_FORMAT(max(p.subscription_date), \"%Y%m\"), DATE_FORMAT(min(p.subscription_date), \"%Y%m\")) + 1) avgPurchase\n" +
+                "from purchaselist p group by p.course_name;");
+
+       
         while (resultSet.next()){
             String coursName = resultSet.getString("name");
             String avgPurchase = resultSet.getString("avgPurchase");
             System.out.println("Среднее количество покупок в месяц, курса: " + coursName + " => " + avgPurchase);
+
         }
+        resultSet.close();
+        statement.close();
+        connection.close();
+
     }
 }
